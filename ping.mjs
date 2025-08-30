b14005a20570df0584f4be04f92634f4a66c45a2
firassattout <firas.sattout@gmail.com>
2025-08-30 22:40:28 +0300

    تحسين

--- ping.mjs
+++ ping.mjs
@@ -7,9 +7,16 @@ async function pingServers() {
     "https://payment-package-1.onrender.com/api/clients/transactionsByProgrammName",
     { headers: { programmName: "s" } }
   );
-  // const res2 = await axios.get(
-  //   "https://payment-package-1.onrender.com/api/clients/getAllCodesAndProgramms"
-  // );
+  const response = await axios.get(
+    "https://projectone-wqlf.onrender.com/api/clients/getCodeByMerchantNumber",
+    {
+      headers: {
+        "Content-Type": "application/json",
+        merchantMSISDN: "+963981922672",
+      },
+      timeout: 50000,
+    }
+  );
   const res3 = await axios.get(
     "https://final-project-mob5.onrender.com/api/get-companyType"
   );
